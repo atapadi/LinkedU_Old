717ca35109a9350bce6c9be173a0ca49748903ea
Akanksha Tapadia <37705676+atapadi@users.noreply.github.com>
2019-04-01 12:56:19 -0500

    View

---
+++ web/resources/js/feed.js
@@ -0,0 +1,23 @@
+$(document).ready(function () 
+{
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () 
+        {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            //var panel = document.getElementsByClassName("panel");
+            if (panel.style.maxHeight) 
+            {
+                panel.style.maxHeight = null;
+            } 
+            else 
+            {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+});
+
